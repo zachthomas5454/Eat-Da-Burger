d964ccdd43eb5831f5bfbdb6e7da73117bd61ee4
Zach <zachariahthomas54@yahoo.com>
2020-01-25 19:01:05 -0500

    Changes to readme

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
-// Serve static content for the app from 'public' directory
+// Serve static content for the app from the 'public' directory
 app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
